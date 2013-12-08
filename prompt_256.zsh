934470a1e19e876771f341f51722e9caa6f5f1a5
Milk Miruku <github@milkmiruku.com>
2013-12-08 09:09:40 +0000

    Update prompt and colours

--- prompt_256.zsh
+++ prompt_256.zsh
@@ -6,7 +6,7 @@ local p="%{$FX[reset]$FG[255]%}"
 local name="%{$FX[reset]$FG[117]%}%n"
 local host="%{$FX[reset]$FG[177]%}%m"
 local jobs="%1(j.(%{$FX[reset]$FG[197]%}%j job%2(j.s.)${p})-.)"
-local time="%{$FX[reset]$FG[215]%}%D{%H:%M}"
+local time="%{$FX[reset]$FG[189]%}%D{%H:%M}"
 local dir="%{$FX[reset]$FG[199]%}%~"
 
 local last="%(?..%{$FX[reset]$FG[203]%}%??${p}:)"
@@ -16,8 +16,8 @@ local priv="%{$FX[reset]$FG[245]%}%#"
 git_branch=""
 
 # PROMPT="${name}${p}@${host}${p}${jobs}${p}:${dir}${p}\${git_branch}${p} %(!.#.$) "
-PROMPT="${name}${p}@${host}${p}${jobs}${p}:${dir}${p}\$(git_super_status)${p} %(!.#.$) "
-RPS1="${time}${p}"
+PROMPT="${time}${p} ${name}${p}@${host}${p}${jobs}${p}:${dir}${p}\$(git_super_status)${p} %(!.#.$) "
+# RPS1=""
 
 #PROMPT="${p}(${name}${p}@${host}${p})-${jobs}(${time}${p})-(${dir}${p}\${git_branch}${p})
 #(${last}${p}${hist}${p}:${priv}${p})- %{$FX[reset]%}"
