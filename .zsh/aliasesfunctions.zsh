53ff47c42234843e95565ec7606df393b75b5e52
Milk Miruku <github@milkmiruku.com>
2017-06-05 21:42:36 +0100

    fix sd for shutdown

--- .zsh/aliasesfunctions.zsh
+++ .zsh/aliasesfunctions.zsh
@@ -223,7 +223,7 @@ alias a='fasd -a'        # any
 alias s='fasd -si'       # show / search / select
 alias d='fasd -d'        # directory
 alias f='fasd -f'        # file
-alias sd='fasd -sid'     # interactive directory selection
+alias fd='fasd -sid'     # interactive directory selection
 alias sf='fasd -sif'     # interactive file selection
 
 # function to execute built-in cd
