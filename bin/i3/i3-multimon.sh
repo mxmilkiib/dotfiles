b8aed4b0678bc578d8f89ca12a7f9cae5520072a
Milk <gitrose@milkmiruku.com>
2018-03-08 00:06:33 +0000

    fix for one screen

--- bin/i3/i3-multimon.sh
+++ bin/i3/i3-multimon.sh
@@ -4,6 +4,8 @@ SWAPOUT="\$o"
 CURRENTDESKOUTPUT=`i3-msg -t get_workspaces | jq -r '.[] | select(.visible and .focused) | .output'`
 if [[ $CURRENTDESKOUTPUT = "DVI-I-1" ]];
 	then OUTPUTID="1";
+elif [[ $CURRENTDESKOUTPUT = "LVDS" ]];
+  then OUTPUTID="1";
 elif [[ $CURRENTDESKOUTPUT = "DVI-I-0" ]];
   then OUTPUTID="2";
 fi
