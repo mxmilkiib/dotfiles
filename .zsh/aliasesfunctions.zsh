e48dc08e26a2a400201dda1e48110db1732a757d
Milk Brewster <github@milkmiruku.com>
2018-07-07 07:59:12 +0100

    better ls aliases

--- .zsh/aliasesfunctions.zsh
+++ .zsh/aliasesfunctions.zsh
@@ -156,14 +156,12 @@ history
 # ls colors for filetype recognition
 if [[ -x "`whence -p dircolors`" ]]; then
 #  eval `dircolors`
- alias ls='ls -F --color=auto'
-else
- alias ls='ls -F'
+ alias ls='ls --color'
 fi
 
-alias ls='ls --color'         									# add colors for filetype recognition
+# alias ls='ls --color'         									# add colors for filetype recognition
 
-alias l='ls -A --group-directories-first'       # show almost all (hidden files)
+alias l='LC_COLLATE=C ls -A --group-directories-first'       # show almost all (hidden files)
 alias lsd='ls --group-directories-first'
 alias lt='ls -Atr --group-directories-first'	  # show almost all (hidden files), sort by time, reversed
 
@@ -172,6 +170,7 @@ alias lla='ls -lhA --group-directories-first'   # long list, alphabetical sort (
 alias laa='ls -lAh --color | less -RFX'				  # long, almost all, no reverse, piped to less w/ redraw (color), quit if under one screen, don't init/deinit terminal
 
 alias llt='ls -ltrh'         										# long list, sort by modification time, reversed (recent last), human readable
+alias llta='ls -ltrhA'        										# long list, sort by modification time, reversed (recent last), human readable, almost all files
 alias llu='ls -lturh'        										# long list, sort by and show access time, reversed, human readable
 alias lc='ls -ltcrh'         										# long list, sort by and show attribute change time, reversed, human readable
 alias lk='ls -lSrh'          										# long list, sort by size, reversed (largest last), human readable
