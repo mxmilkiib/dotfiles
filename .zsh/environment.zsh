365a9963cdb70dde3a1b88b26ae4a995f5fcb8b5
Milk Brewster <githubpublic@milkmiruku.com>
2018-08-09 16:27:33 +0100

    fix .cache

--- .zsh/environment.zsh
+++ .zsh/environment.zsh
@@ -19,7 +19,7 @@ export TZ=:/etc/localtime
 # xdg standard
 export XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-"$HOME/.config"}
 export XDG_DATA_HOME=${XDG_DATA_HOME:-"$HOME/.local/share"}
-export XDG_CACHE_HOME=${XDG_CACHE_HOME:-"/var/cache"}
+export XDG_CACHE_HOME=${XDG_CACHE_HOME:-"$HOME/.cache"}
 
 # gives fuller GUI 
 export XDG_CURRENT_DESKTOP=XFCE
