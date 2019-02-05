0812f9e2a64049ee0b6a6eec46602c1c503b2b9f
Milk Brewster <github@milkmiruku.com>
2019-02-05 13:51:19 +0000

    fix multimon

--- bin/i3/i3-multimon.sh
+++ bin/i3/i3-multimon.sh
@@ -5,7 +5,7 @@ if [ `ps aux | grep -c sway` > 1 ]; then wmm="swaymsg"; wms="sway"; else wmm="i3
 CURRENTDESKOUTPUT=`$wmm -t get_workspaces | jq -r '.[] | select(.visible and .focused) | .output'`
 if [[ $CURRENTDESKOUTPUT = DVI-I-1 ]] || [[ $CURRENTDESKOUTPUT = LVDS* ]];
   then OUTPUTID="1";
-elif [[ $CURRENTDESKOUTPUT = DVI-I-0 ]] || [[ $CURRENTDESKOUTPUT = VGA* ]];
+elif [[ $CURRENTDESKOUTPUT = DVI-I-0 ]] || [[ $CURRENTDESKOUTPUT = VGA* ]] || [[ $CURRENTDESKOUTPUT = HDMI* ]];
   then OUTPUTID="2";
 fi
 NEWARGS="${ARGS//\$o/$OUTPUTID}"
