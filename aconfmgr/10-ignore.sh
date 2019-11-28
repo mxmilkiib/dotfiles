6d4f1fad7854cc55eeb1e344bc45557d8322b928
Milk Brewster <github@milkmiruku.com>
2019-11-28 23:55:36 +0000

    aconfmgr things

--- aconfmgr/10-ignore.sh
+++ aconfmgr/10-ignore.sh
@@ -1,3 +1,20 @@
+# aconfmgr ignores of Milk Brewster mxmilkb
+IgnorePath '/lost+found'
+
+IgnorePath '*.cache'
+IgnorePath '*pycache*'
+# IgnorePath '*/__pycache__/*'
+IgnorePath '*/*.pyc'
+
+
+IgnorePath '/boot/*.img'
+IgnorePath '/boot/loader/random-seed'
+IgnorePath '/boot/vmlinuz*'
+IgnorePath '/boot/EFI/*'
+
+
+IgnorePath '/etc/.updated'
+IgnorePath '/etc/*-'
 IgnorePath '/etc/ca-certificates/*'
 IgnorePath '/etc/ssl/*'
 IgnorePath '/etc/lvm/archive/*'
@@ -5,6 +22,19 @@ IgnorePath '/etc/ssh/*'
 IgnorePath '/etc/laptop-mode'
 IgnorePath '/etc/pacman.d/gnupg/*'
 IgnorePath '/etc/udev/hwdb.bin'
+IgnorePath '/etc/netctl/*'
+IgnorePath '/etc/ca-certificates/*'
+IgnorePath '/etc/cups/*'
+IgnorePath '/etc/gconf/gconf.xml.defaults/*'
+IgnorePath '/etc/ld.so.cache'
+IgnorePath '/etc/docker/key.json'
+IgnorePath '/etc/nginx/*'
+IgnorePath '/etc/pacman.d/gnupg/*'
+IgnorePath '/etc/sgml/catalog'
+IgnorePath '/etc/ssh/ssh_host*'
+IgnorePath '/etc/ssl/certs/*'
+IgnorePath '/etc/udev/hwdb.bin'
+IgnorePath '/etc/xml/catalog'
 
 
 # IgnorePath '/usr/lib/modules*'
@@ -13,12 +43,12 @@ IgnorePath '/etc/udev/hwdb.bin'
 # IgnorePath '/usr/lib/ghc*'
 # IgnorePath '/usr/lib/python*'
 # IgnorePath '/usr/lib/ruby*'
-
 IgnorePath '/usr/lib/*'
 
 IgnorePath '/usr/share/webapps/*'
 IgnorePath '/usr/share/fonts/*'
 IgnorePath '/usr/share/.mono/*'
+IgnorePath '/usr/share/mime/*'
 
 IgnorePath '/usr/local/*'
 
@@ -33,16 +63,7 @@ IgnorePath '/usr/local/*'
 # IgnorePath '/var/www/*'
 # IgnorePath '/var/spool/*'
 # IgnorePath '/var/aconfmgr/*'
-
 IgnorePath '/var/*'
-
-
-IgnorePath '/boot/*'
-
-
-IgnorePath '*.cache'
-IgnorePath '*pycache*'
-IgnorePath '*pyc'
 
 
 IgnorePath '/opt/quake/*'
