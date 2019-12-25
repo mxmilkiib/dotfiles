8bd18653570d5e97ab06a859237a203eb130de3d
Milk Brewster <github@milkmiruku.com>
2019-12-25 05:33:21 +0000

    bspcmw layout updates

--- bin/bin/bspwm_grid.sh
+++ bin/bin/bspwm_grid.sh
@@ -3,10 +3,10 @@
 node_number=$(bspc query --nodes --desktop --node .window | wc -l)
 
 case $node_number in
-	1) echo "split_dir=east";;
-	2) bspc node -f biggest.local && echo "split_dir=south";;
+	1) echo "split_dir=south";;
+	2) bspc node -f biggest.local && bspc node -f south && echo "split_dir=east";;
 	# 3) echo "split_dir=south";;
-	3) bspc node -f biggest.local && echo "split_dir=south";;
+	3) bspc node -f biggest.local && echo "split_dir=east";;
 	# 4) echo "bspc desktop -f next";;
 	4) bspc node -f biggest.local && bspc node -f south && echo "split_dir=south";;
 	5) bspc node -f biggest.local && bspc node -f east && echo "split_dir=south";;
