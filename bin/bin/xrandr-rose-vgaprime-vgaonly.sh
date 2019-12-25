eff2a7f0b1f2471f41e9ff0d517cfb1e7b19ba7d
Milk Brewster <github@milkmiruku.com>
2019-12-25 03:20:39 +0000

    scripts

--- bin/bin/xrandr-rose-vgaprime-vgaonly.sh
+++ bin/bin/xrandr-rose-vgaprime-vgaonly.sh
@@ -2,4 +2,4 @@
 killall polybar
 xrandr --output LVDS1 --off --output DP1 --off --output HDMI1 --off --output VGA1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
 feh --bg-fill --no-xinerama ~/.wallpapers/danielle_at_sea_flickr.jpg
-polybar sitting-tray &
+nohup polybar sitting-tray &! >/dev/null 2>&1
