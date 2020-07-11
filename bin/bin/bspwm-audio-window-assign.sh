b57852ea293f9fa30b47630f79b1cfaf58afe7ea
Milk Brewster <github@milkmiruku.com>
2020-07-11 16:00:50 +0100

    bspwm rules

--- bin/bin/bspwm-audio-window-assign.sh
+++ bin/bin/bspwm-audio-window-assign.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-bspc rule -a shuriken --one-shot desktop=^7
+bspc rule -a qseq66 --one-shot desktop=^5
 bspc rule -a Carla2 --one-shot desktop=^6
+bspc rule -a shuriken --one-shot desktop=^7
 
